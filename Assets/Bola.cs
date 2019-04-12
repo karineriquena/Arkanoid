4f4cd1f38aa94146c341003ba5b8a655789a8808
Karine Riquena Limp <karine.limp@totvs.com.br>
2019-04-12 11:50:38 -0300

    Class 1 to 6

---
+++ Assets/Bola.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+    // public Vector2 Velocidade;
+    public Vector2 Velocidade;
+    
+    void Start() {
+
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D> ();
+        rigidbody.AddForce (Velocidade);
+
+    }
+
+}
